b856f1fa05b873c770688f2f378674708a6e17f4
anthonykool <anthonytcool@hotmail.com>
2026-06-18 17:10:56 +0700

    refactor: update product type for category relation

--- types/index.ts
+++ types/index.ts
@@ -12,11 +12,13 @@ import {
 
 export type Product = z.infer<typeof insertProductSchema> & {
   id: string;
-  rating: string;
-  numReviews: number;
   createdAt: Date;
+  category?: {
+    id: string;
+    name: string;
+    slug: string;
+  };
 };
-
 export type Cart = z.infer<typeof insertCartSchema>;
 export type CartItem = z.infer<typeof cartItemSchema>;
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
