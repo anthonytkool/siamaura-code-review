19995c9cbc913f8c0665889d732baf44cae79618
anthonykool <anthonytcool@hotmail.com>
2026-06-20 12:46:46 +0700

    style: align view all products button with brand theme

--- components/view-all-products-button.tsx
+++ components/view-all-products-button.tsx
@@ -4,7 +4,10 @@ import Link from 'next/link';
 const ViewAllProductsButton = () => {
   return (
     <div className='flex justify-center items-center my-8'>
-      <Button asChild className='px-8 py-4 text-lg font-semibold'>
+      <Button
+        asChild
+        className='bg-[#b38b2d] px-8 py-4 text-lg font-semibold text-white hover:bg-[#9f7a24]'
+      >
         <Link href='/search'>View All Products</Link>
       </Button>
     </div>
