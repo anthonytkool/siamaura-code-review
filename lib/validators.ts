e98be69c880ad92a76b8f825b24631b126d6f1a2
anthonykool <anthonytcool@hotmail.com>
2026-06-24 22:37:53 +0700

    fix: typo leaast and email validation in updateProfileSchema

--- lib/validators.ts
+++ lib/validators.ts
@@ -130,8 +130,8 @@ export const paymentResultSchema = z.object({
 
 // Schema for updating the user profile
 export const updateProfileSchema = z.object({
-  name: z.string().min(3, 'Name must be at leaast 3 characters'),
-  email: z.string().min(3, 'Email must be at leaast 3 characters'),
+  name: z.string().min(3, 'Name must be at least 3 characters'),
+  email: z.string().email('Invalid email address'),
 });
 
 // Schema to update users
